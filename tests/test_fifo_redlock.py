6af5b54439662ed3d5500e2aa9e4164aacbe7010
Félix Bouliane <fbouliane@internap.com>
2016-01-25 16:36:17 -0500

    Make sure the test simulates real-world.
    Make the connector is global for every lock request.
    Before, the connector was local to a thread, with independant redis mock.
    Therefore, no concurrent testing was done.

--- tests/test_fifo_redlock.py
+++ tests/test_fifo_redlock.py
@@ -28,13 +28,12 @@ class FIFORedlockTest(test_extensible_redlock.ExtensibleRedlockTest):
                 output.append(thread_name)
                 sleep(delay_before_releasing_lock)
                 lock_source.unlock(lock)
+        connector = FIFORedlock(get_servers_pool(active=1, inactive=0))
 
         for t in thread_names:
-            connector = FIFORedlock(get_servers_pool(active=1, inactive=0))
-
             simulate_work_delay = 0.02
             thread = threading.Thread(target=get_lock_and_register, args=(t, connector, 'pants', threads_that_got_the_lock,
-                                                                          simulate_work_delay))
+                                      simulate_work_delay))
             sleep(0.01)
             thread.start()
             threads.append(thread)
